52c3bbfbb215665b2832545deaee6979edf77dd9
Tim Hobson <claehobson@gmail.com>
2020-04-21 22:32:45 +1000

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://timmy:timmy123@mernshopping-bdie6.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(express.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on ${port}`));
+
