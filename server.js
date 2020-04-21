b405a8d024ba563430123f0c1eb56b9994fd4474
Tim Hobson <claehobson@gmail.com>
2020-04-21 23:04:36 +1000

    backend created, get post delete.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser'); // not needed anymore?
+
+const items = require('./routes/api/items');
 
 const app = express();
 
@@ -20,7 +22,9 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
+// Use Routes
+app.use('/api/items', items);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on ${port}`));
-
