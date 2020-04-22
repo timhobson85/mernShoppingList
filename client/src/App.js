0b7aa2e93d83f9bc6f99f5072fff5e0a6ab94b0a
Tim Hobson <claehobson@gmail.com>
2020-04-23 00:38:01 +1000

    basic redux implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import AppNavBar from './components/AppNavBar'
-import ShoppingList from './components/ShoppingList'
+import AppNavBar from './components/AppNavBar';
+import ShoppingList from './components/ShoppingList';
+import store from './store';
+
+import { Provider } from 'react-redux';
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-       <h1>Hello</h1>
-        <AppNavBar />
-        <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Hello</h1>
+          <AppNavBar />
+          <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 
