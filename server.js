1993b126b01d1b2bce0d20e3f27461167c519f8e
Tim Hobson <claehobson@gmail.com>
2020-04-24 00:55:34 +1000

    changing app.user

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/api/items', items);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.user(express.static('client/build'));
+  // app.user(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
