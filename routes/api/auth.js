7597a84c18b2136b4d58c1a506b2216414ee4e44
Tim Hobson <claehobson@gmail.com>
2020-04-25 00:13:17 +1000

    auth middleware working

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -50,4 +50,13 @@ router.post('/', (req, res) => {
   
 });
 
+// @route   GET api/auth/user
+// @desc    Get User Data
+// @access  Private
+router.get('/user', auth, ( req, res ) => {
+  User.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user));
+});
+
 module.exports = router;
