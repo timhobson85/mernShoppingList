83333c9bd51a32f1c3c3be3babcb4c8dcca52a20
Tim Hobson <claehobson@gmail.com>
2020-04-24 15:51:16 +1000

    changing item.js back to Item.js

--- routes/api/items.js
+++ routes/api/items.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Item Model
-const Item = require('../../models/item.js');
+const Item = require('../../models/Item');
 
 // @route   GET api/items
 // @desc    Get All Items
