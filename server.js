59ef286eeb8afedfe14a329565463bb787767883
Tim Hobson <claehobson@gmail.com>
2020-04-24 15:55:14 +1000

    changing item.js back to Item.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 // const bodyParser = require('body-parser'); // not needed anymore?
 const path = require('path');
 
-const items = require('./routes/api/items');
-
 const app = express();
 
 // Bodyparser Middleware
@@ -24,7 +22,8 @@ mongoose
   .catch(err => console.log(err));
 
 // Use Routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
