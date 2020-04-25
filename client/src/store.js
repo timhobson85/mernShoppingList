c3210bc94aa1d79603e43b5816a63e996d0224dc
Tim Hobson <claehobson@gmail.com>
2020-04-26 00:30:39 +1000

    disable redux for heroku

--- client/src/store.js
+++ client/src/store.js
@@ -10,8 +10,8 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // disable for heroku
+    applyMiddleware(...middleware)//,
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // disable for heroku
   )
 );
 
